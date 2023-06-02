a72ad434f58783be08974c218849ef03b7cccca0
kCyp <cypoye@gmail.com>
2023-06-03 01:50:46 +0200

    Cooldown added in Skills

--- Shared/GameTimelinePlanner.Shared.Domain/Entity/Skill.cs
+++ Shared/GameTimelinePlanner.Shared.Domain/Entity/Skill.cs
@@ -4,16 +4,18 @@ namespace GameTimelinePlanner.Shared.Domain.Entity;
 
 public class Skill : IDisplayable
 {
-    public Skill(string name, int duration, string description, DisplayDescription displayDescription)
+    public Skill(string name, int duration, int cooldown, string description, DisplayDescription displayDescription)
     {
         Name = name;
         Duration = duration;
+        Cooldown = cooldown;
         Description = description;
         DisplayDescription = displayDescription;
     }
 
     public string Name { get; private init; }
     public int Duration { get; private init; }
+    public int Cooldown { get; private init; }
     public string Description { get; private init; }
     public DisplayDescription DisplayDescription { get; init; }
 }

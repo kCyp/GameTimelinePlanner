b7badd632b752f3b6f2e21571eb28bb35cc67b1d
kCyp <cypoye@gmail.com>
2023-06-03 14:56:55 +0200

    Added Role class, and new fields on Entities

--- Shared/GameTimelinePlanner.Shared.Domain/Entity/Role.cs
+++ Shared/GameTimelinePlanner.Shared.Domain/Entity/Role.cs
@@ -1,8 +1,29 @@
 ﻿namespace GameTimelinePlanner.Shared.Domain.Entity;
 
-public enum Role
+public enum RoleType
 {
     Tank,
     Healer,
-    DamageDealer
+    Melee,
+    MagicalRange,
+    PhysicalRange
+}
+
+public class Role
+{
+    public RoleType Type { get; set; }
+    public string Name { get; set; }
+    public IList<Skill> Skills { get; set; } = new List<Skill>();
+
+    public Role(RoleType type, string name, IList<Skill> skills)
+    {
+        Type = type;
+        Name = name;
+        Skills = skills;
+    }
+
+    public override int GetHashCode()
+    {
+        return Type.GetHashCode();
+    }
 }
--- Shared/GameTimelinePlanner.Shared.Domain/Entity/Skill.cs
+++ Shared/GameTimelinePlanner.Shared.Domain/Entity/Skill.cs
@@ -4,9 +4,10 @@ namespace GameTimelinePlanner.Shared.Domain.Entity;
 
 public class Skill : IDisplayable
 {
-    public Skill(string name, int duration, int cooldown, string description, DisplayDescription displayDescription)
+    public Skill(string name, int duration, int cooldown, string description, DisplayDescription displayDescription, int requiredLevel)
     {
         Name = name;
+        RequiredLevel = requiredLevel;
         Duration = duration;
         Cooldown = cooldown;
         Description = description;
@@ -14,6 +15,7 @@ public class Skill : IDisplayable
     }
 
     public string Name { get; private init; }
+    public int RequiredLevel { get; private init; }
     public int Duration { get; private init; }
     public int Cooldown { get; private init; }
     public string Description { get; private init; }

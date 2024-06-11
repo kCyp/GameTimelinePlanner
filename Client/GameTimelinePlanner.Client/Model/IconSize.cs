2778d5667628d7d9fdf35a8b2fc3896f8384d304
kCyp <cypoye@gmail.com>
2024-06-11 03:17:38 +0200

    Add size selection on icon component, add icon of skill on usage in timeline

---
+++ Client/GameTimelinePlanner.Client/Model/IconSize.cs
@@ -0,0 +1,24 @@
+﻿namespace GameTimelinePlanner.Client.Model;
+
+public enum IconSize
+{
+    Small,
+    Medium,
+    Large,
+    Auto
+}
+
+public static class IconSizeHelper
+{
+    public static string ToCssClass(this IconSize? iconSize)
+    {
+        return iconSize switch
+        {
+            IconSize.Small => "small",
+            IconSize.Medium => "medium",
+            IconSize.Large => "large",
+            IconSize.Auto => "auto",
+            _ => "",
+        };
+    }
+}
